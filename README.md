46f99ac566c9daaac5f3054e4a187f09de1e9230
fredblade <fredblade142@gmail.com>
2023-04-11 16:02:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-yo7rjq)
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  modules: ['@nuxt/content'],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+});
