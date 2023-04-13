32ded6a062df65ab9ad4bd7c319703bb30e5ab4f
fred <fredblade142@gmail.com>
2023-04-12 22:01:44 -0400

    ythtrkf;eytrhkdfnm.sey8hgnv htuiy45wieh

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,4 +2,18 @@
   <main>
     <ContentDoc />
   </main>
+  <article>
+    <nuxt-content :document="article" />
+    article xd
+  </article>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('/', params.slug).fetch()
+    console.log(article);
+    return { article }
+  }
+}
+</script>
